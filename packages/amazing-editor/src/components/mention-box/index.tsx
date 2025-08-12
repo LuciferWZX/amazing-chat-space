ae6aa926435ba08ca93847905bc2b76d9fc52a18
wuzhixin <2396423791@qq.com>
2025-08-13 00:26:13 +0800

    feat:优化mention

--- packages/amazing-editor/src/components/mention-box/index.tsx
+++ packages/amazing-editor/src/components/mention-box/index.tsx
@@ -1,41 +1,47 @@
-import type { MentionConfig, MentionDataItem } from "@/core"
-import { cn, ScrollArea } from "@amazing-chat/ui"
-import { useCallback, useEffect, useState, type ComponentProps } from "react"
-import MentionItem from "./mention-item"
+import type { MentionConfig, MentionDataItem } from '@/core'
+import { cn, ScrollArea } from '@amazing-chat/ui'
+import {type ComponentProps, useCallback} from 'react'
+import MentionItem from './mention-item'
+import Highlighter from "react-highlight-words";
 
-interface MentionBoxProps extends ComponentProps<"ul"> {
-    config:MentionConfig
-    keyword:string
+interface MentionBoxProps extends ComponentProps<'ul'> {
+  config: MentionConfig
+  keyword: string
+  items: MentionDataItem[]
+  activeKey: string | null
 }
-export const MentionBox = (props:MentionBoxProps) => {
-    const {className,config,keyword,...rest}=props
-    const [items,setItems]=useState<MentionDataItem[]>([])
-    
-    const fetchData=useCallback(async ()=>{
-        if(config.fetch){
-            const data= await config.fetch(keyword)
-            setItems(data)
-        }
-        setItems(config?.data ?? [])
-    },[config.data,config.fetch,keyword])
-    useEffect(()=>{
-        fetchData()
-    },[config.data,config.fetch,keyword])
-    return(
-        <ul className={cn("absolute top-0 left-0 z-0 w-[200px] bg-card border border-border rounded-md shadow-md",className)} {...rest}>
-          <ScrollArea classes={{viewport:"max-h-[200px] p-3"}}>
-            {items.map(item=>{
-                return(
-                    <MentionItem 
-                        key={item.value} 
-                        disabled={item.disabled}
-                        className={config.className}
-                        label={item.label} >
-                        {config.renderItem?config.renderItem(item):item.label}
-                    </MentionItem>
-                )
-            })}
-          </ScrollArea>
-        </ul>
+export const MentionBox = (props: MentionBoxProps) => {
+  const { className, config, keyword, activeKey, items, ...rest } = props
+  const highlightWrapper = useCallback((label:string)=>{
+    return (
+        <Highlighter
+            highlightClassName={"bg-[unset] text-primary"}
+            searchWords={[keyword]}
+            autoEscape={true}
+            textToHighlight={label}
+        />
     )
+  },[keyword])
+  return (
+    <ul
+      className={cn('absolute top-0 left-0 z-0 w-[200px] bg-card border border-border rounded-md shadow-md', className)}
+      {...rest}
+    >
+      <ScrollArea classes={{ viewport: 'max-h-[200px] p-3' }}>
+        {items.map(item => {
+          return (
+            <MentionItem
+              key={item.value}
+              disabled={item.disabled}
+              className={config.className}
+              label={item.label}
+              active={activeKey === item.value}
+            >
+              {config.renderItem ? config.renderItem(item) : highlightWrapper(item.label)}
+            </MentionItem>
+          )
+        })}
+      </ScrollArea>
+    </ul>
+  )
 }
--- packages/amazing-editor/src/hooks/use-mention-selection.tsx
+++ packages/amazing-editor/src/hooks/use-mention-selection.tsx
@@ -1,95 +1,169 @@
-import { MentionBox, Portal } from "@/components"
-import type { MentionConfig } from "@/core"
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { Editor, Node, Range } from "slate"
-import { ReactEditor } from "slate-react"
-const useMentionSelection=(editor:Editor,mentions?:Array<MentionConfig>)=>{
-    const [targetRange,setTargetRange]=useState<Range|null>(null)
-    const [keyword,setKeyword]=useState<string>("")
-    const [mentionConfig,setMentionConfig]=useState<MentionConfig|null>(null)
-    const mentionNodeRef = useRef<HTMLUListElement|null>(null)
-   const onChangeWithMention=()=>{
-    if (!mentions || mentions.length === 0) {
-        return
-    }
-    const {selection}=editor
-    if (selection && Range.isCollapsed(selection)) {
-        const {anchor}=selection
-        const [startNode]=Editor.node(editor,anchor)    
-        const textBeforeCursor = Node.string(startNode).slice(0,anchor.offset)
-        console.warn("textBeforeCursor:",textBeforeCursor);
-        let atIndex = -1
-        let mentionConfig:MentionConfig|null=null
-        for (const mention of mentions) {
-            const curIndex=textBeforeCursor.lastIndexOf(mention.trigger)
-            if (curIndex === -1) {
-                //说明没找到关键词
-                continue
-            }
-              //说明找到关键词了
-            atIndex=curIndex
-            mentionConfig=mention
-            break;   
-        }
-        if (atIndex !== -1) {
-            const searchText=textBeforeCursor.slice(atIndex)
-            const mentionRange = Editor.range(editor,{...anchor,offset:atIndex},anchor)
-            setTargetRange(mentionRange)
-            setKeyword(searchText)
-            setMentionConfig(mentionConfig)
-            // editor.selection=mentionRange    
-        }else{
-            setTargetRange(null)
-            setKeyword("")
-            setMentionConfig(null)
-        }
-    }
-   }
-   const onKeydownWithMention=useCallback((e:React.KeyboardEvent<HTMLDivElement>)=>{
-    if(e.key==="Enter"){
-        e.preventDefault()
-        e.stopPropagation()
-    }
-   },[keyword,editor,targetRange,mentionConfig])
-   useEffect(()=>{
-     if (targetRange && mentionConfig) {
-        const el = mentionNodeRef.current
-        if (el) {
-            const domRange=ReactEditor.toDOMRange(editor,targetRange)
-           const rect=domRange.getBoundingClientRect()
-           el.style.left = `${rect.left + window.pageXOffset}px`
-           if ((rect.bottom+el.offsetHeight) > window.innerHeight) {
-            el.style.top = `${rect.bottom - el.offsetHeight - rect.height}px`
-           }else{
-            el.style.top = `${rect.top + window.pageYOffset+24}px`
-           }
-           if (rect.left+el.offsetWidth > window.innerWidth) {
-            el.style.left = `${rect.right - el.offsetWidth - rect.width}px`
-           }else{
-            el.style.left = `${rect.left + window.pageXOffset}px`
-           }
+import { MentionBox, Portal } from '@/components'
+import type { MentionConfig, MentionDataItem } from '@/core'
+import { isHotkey } from 'is-hotkey'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { Editor, Node, Range } from 'slate'
+import { ReactEditor } from 'slate-react'
 
-           el.style.zIndex = '100'
-        }
-     }   
-   },[targetRange,editor,keyword,mentionConfig])
-   const mentionNode = useMemo(()=>{
-    if (mentionConfig && targetRange) {
-        return (
-            <Portal>
-                <MentionBox 
-                    config={mentionConfig}
-                    keyword={keyword}
-                    ref={mentionNodeRef} 
-                    />
-            </Portal>
-        )
+const useMentionSelection = (editor: Editor, mentions?: Array<MentionConfig>) => {
+  const [targetRange, setTargetRange] = useState<Range | null>(null)
+  const [keyword, setKeyword] = useState<string>('')
+  const [mentionConfig, setMentionConfig] = useState<MentionConfig | null>(null)
+  const mentionNodeRef = useRef<HTMLUListElement | null>(null)
+  const [items, setItems] = useState<MentionDataItem[]>([])
+  const [activeKey, setActiveKey] = useState<string | null>(null)
+  const fetchData = useCallback(async () => {
+    let data = mentionConfig?.fetch ? await mentionConfig.fetch(keyword) : (mentionConfig?.data ?? [])
+    if (data.length > 0) {
+      data = [{ label: '@所有人', value: '@all' }, ...data]
+      data = data.filter(item => item.label.startsWith(keyword))
     }
-   },[mentionConfig,targetRange,keyword])
-   return {
+    if (data.length > 0) {
+      setActiveKey(data[0].value)
+    }
+    setItems(data)
+  }, [mentionConfig?.data, mentionConfig?.fetch, keyword])
+  const onChangeWithMention = () => {
+    if (!mentions || mentions.length === 0) {
+      return
+    }
+    const { selection } = editor
+    if (selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection
+      const [startNode] = Editor.node(editor, anchor)
+      const textBeforeCursor = Node.string(startNode).slice(0, anchor.offset)
+      let atIndex = -1
+      let mentionConfig: MentionConfig | null = null
+      for (const mention of mentions) {
+        const curIndex = textBeforeCursor.lastIndexOf(mention.trigger)
+        if (curIndex === -1) {
+          //说明没找到关键词
+          continue
+        }
+        //说明找到关键词了
+        atIndex = curIndex
+        mentionConfig = mention
+        break
+      }
+      if (atIndex !== -1) {
+        const searchText = textBeforeCursor.slice(atIndex)
+        const mentionRange = Editor.range(editor, { ...anchor, offset: atIndex }, anchor)
+        setTargetRange(mentionRange)
+        //searchText去除前面的trigger
+        const keywordWithoutTrigger = searchText.slice(mentionConfig?.trigger.length ?? 0)
+        setKeyword(keywordWithoutTrigger)
+        setMentionConfig(mentionConfig)
+        // editor.selection=mentionRange
+      } else {
+        setTargetRange(null)
+        setKeyword('')
+        setMentionConfig(null)
+      }
+    }
+  }
+  const getNextItemKey = (active: string | null, data: MentionDataItem[]) => {
+    if (data.length === 0) {
+      return null
+    }
+    if (active === null) {
+      return data[0].value
+    }
+    const index = data.findIndex(item => item.value === active)
+    if (index === -1 || index === data.length - 1) {
+      return data[0].value
+    }
+    const target = data[index + 1]
+    if (target?.disabled) {
+      return getNextItemKey(target.value, data)
+    }
+    return target.value
+  }
+  const getPrevItemKey = (active: string | null, data: MentionDataItem[]) => {
+    if (active === null) {
+      return data[0].value
+    }
+    const index = data.findIndex(item => item.value === active)
+    if (index === -1) {
+      return data[0].value
+    }
+    if (index === 0) {
+      return data[data.length - 1].value
+    }
+    const target = data[index - 1]
+    if (target?.disabled) {
+      return getPrevItemKey(target.value, data)
+    }
+    return target.value
+  }
+  const onKeydownWithMention = useCallback(
+    (e: React.KeyboardEvent<HTMLDivElement>) => {
+
+      if (items.length>0 && targetRange){
+        if (isHotkey('up', e)) {
+          e.preventDefault()
+          console.warn('up')
+          const nextKey = getPrevItemKey(activeKey, items)
+          setActiveKey(nextKey)
+        }
+        if (isHotkey('down', e)) {
+          e.preventDefault()
+          console.warn('down')
+          const nextKey = getNextItemKey(activeKey, items)
+          setActiveKey(nextKey)
+        }
+      }
+
+    },
+    [editor, items, activeKey,targetRange],
+  )
+  //获取下拉框数据
+  useEffect(() => {
+    fetchData()
+  }, [mentionConfig, keyword])
+  //渲染下拉框
+  useEffect(() => {
+    if (targetRange && mentionConfig && items && items.length > 0) {
+      const el = mentionNodeRef.current
+      if (el) {
+        const domRange = ReactEditor.toDOMRange(editor, targetRange)
+        const rect = domRange.getBoundingClientRect()
+        el.style.left = `${rect.left + window.pageXOffset}px`
+        if (rect.bottom + el.offsetHeight > window.innerHeight) {
+          el.style.top = `${rect.bottom - el.offsetHeight - rect.height}px`
+        } else {
+          el.style.top = `${rect.top + window.pageYOffset + 24}px`
+        }
+        if (rect.left + el.offsetWidth > window.innerWidth) {
+          el.style.left = `${rect.right - el.offsetWidth - rect.width}px`
+        } else {
+          el.style.left = `${rect.left + window.pageXOffset}px`
+        }
+
+        el.style.zIndex = '100'
+      }
+    }
+  }, [targetRange, editor, mentionConfig, items])
+
+  //下拉框节点
+  const mentionNode = useMemo(() => {
+    if (mentionConfig && targetRange && items && items.length > 0) {
+      return (
+        <Portal>
+          <MentionBox
+            activeKey={activeKey}
+            items={items}
+            config={mentionConfig}
+            keyword={keyword}
+            ref={mentionNodeRef}
+          />
+        </Portal>
+      )
+    }
+  }, [mentionConfig, targetRange, items, keyword, activeKey])
+  return {
     onChangeWithMention,
     mentionNode,
-    onKeydownWithMention
-   }
+    onKeydownWithMention,
+  }
 }
 export default useMentionSelection
